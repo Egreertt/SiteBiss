87a83dbea699ddfa96e05dd6337450702198e57c
Egreertt <egreertt@gmail.com>
2024-06-04 21:24:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-document.getElementById('showAdButton').addEventListener('click', function() {
-    const AdController = window.Adsgram.init({ blockId: "102", debug: true });
 
-    AdController.show().then((result) => {
-        console.log('Реклама показана полностью');
-    }).catch((result) => {
-        console.log('Ошибка или реклама пропущена');
-    });
-});
